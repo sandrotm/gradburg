d48b025e4485b6d6d2e255b5601ac070fe763105
sandro tm <sandromail@gmail.com>
2015-12-01 09:32:38 +0400

    somechanges

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,10 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
-    url(r'^nadzaladevi/$', views.nadzaladevi, name='nadzaladevi'),
+    url(r'^$', views.nadzaladevi, name='nadzaladevi'),
+    #url(r'^nadzaladevi/$', views.nadzaladevi, name='nadzaladevi'),
     url(r'^aboutUs/$', views.aboutUs, name='aboutUs'),
     url(r'^contact/$', views.contact, name='contact'),
+    url(r'^process/$', views.process, name='process'),
+    url(r'^flats/$', views.flats, name='flats'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from .models import Flat, FlatType, Image, Page
 
+# not used currently
 def home(request):
     context = {}
     return render(request, 'core/pages/home.html', context)
 
 def nadzaladevi(request):
+    context = {}
+    return render(request, 'core/pages/nadzaladevi.html', context)
+
+# not used currently
+def aboutUs(request):
+    context = {}
+    context['images'] = Image.objects.filter(title__contains='ძველი')
+    return render(request, 'core/pages/aboutUs.html', context)
+
+def process(request):
+    context = {}
+    context['images'] = Image.objects.filter(title__contains='პროცესი')
+    return render(request, 'core/pages/process.html', context)
+
+def flats(request):
     context = {}
     context['flats'] = Flat.objects.all()
     context['flat_group1'] = Flat.objects.filter(type__title='მცირე ბინა').order_by('-floor')
@@ -14,12 +30,7 @@ def nadzaladevi(request):
     context['flat_group3'] = Flat.objects.filter(type__title='დიდი ბინა').order_by('-floor')
     context['flat_types'] = FlatType.objects.all()
     context['floors'] = list(range(8, 0, -1))
-    return render(request, 'core/pages/nadzaladevi.html', context)
-
-def aboutUs(request):
-    context = {}
-    context['images'] = Image.objects.all()
-    return render(request, 'core/pages/aboutUs.html', context)
+    return render(request, 'core/pages/flats.html', context)
 
 def contact(request):
     context = {}
